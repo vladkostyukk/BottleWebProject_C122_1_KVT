bf6dc408477270cba3928f0048ad905ae6ad46af
Sone4ka <sone4ka-05@yandex.ru>
2024-05-17 02:20:27 +0300

    Добавление ограничений на ввод матрицы весов рёбер

--- BottleWebProject_C122_1_KVT/static/scripts/matrix_kruskal.js
+++ BottleWebProject_C122_1_KVT/static/scripts/matrix_kruskal.js
@@ -1,4 +1,3 @@
-
 function generateMatrix() {
     const n = parseInt(document.getElementById('vertices-input').value);
     let matrix = '<table>';
@@ -13,13 +12,36 @@ function generateMatrix() {
     for (let i = 0; i < n; i++) {
         matrix += `<tr><td class="bfs-td" style="width: 30px; text-align: center;">${i}</td>`; // Создаем боковую шапку с номерами строк 
         for (let j = 0; j < n; j++) {
-            matrix += `<td><input class="input_bfs rounded-input" type="number" min="0" value="0" id="cell-${i}-${j}" style="width: 100px;"></td>`;
+            if (i === j) { // Для диагональных элементов
+                matrix += `<td><input class="input_bfs rounded-input" type="number" min="0" value="0" id="cell-${i}-${j}" style="width: 100px;" readonly></td>`;
+            } else {
+                matrix += `<td><input class="input_bfs rounded-input" type="number" min="0" value="0" id="cell-${i}-${j}" style="width: 100px;"></td>`;
+            }
         }
         matrix += "</tr>";
     }
 
     matrix += '</table>';
     document.getElementById('matrix_kruskal').innerHTML = matrix;
+
+    // Добавляем обработчик событий для изменения значений в ячейках
+    var inputs = document.querySelectorAll('.input_bfs');
+    inputs.forEach(input => {
+        input.addEventListener('change', function (event) {
+            // Получаем значение из измененной ячейки
+            var value = event.target.value;
+            // Получаем индексы строки и столбца измененной ячейки
+            var rowIndex = parseInt(event.target.id.split('-')[1]);
+            var colIndex = parseInt(event.target.id.split('-')[2]);
+            // Если измененная ячейка находится на диагонали, оставляем ее значение равным нулю и прерываем обработку
+            if (rowIndex === colIndex) {
+                event.target.value = 0;
+                return;
+            }
+            // Устанавливаем значение в соответствующей симметричной ячейке
+            document.getElementById(`cell-${colIndex}-${rowIndex}`).value = value;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
